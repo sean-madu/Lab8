3c518459620bc504052803292852fb0b521d27c6
sean-madu <madusean2@gmail.com>
2023-03-09 17:55:26 -0700

    Count test will pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -46,7 +46,7 @@ public class CustomList extends ArrayAdapter<City> {
     }
 
     public int getCount(){
-        return -1;
+        return cities.size();
     }
 
     public void addCity(City city){
